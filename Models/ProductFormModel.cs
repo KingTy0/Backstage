54a05e246c168d675f4a68d8425f10b59f2b94a2
King <King10@colum.edu>
2026-03-30 13:34:19 -0500

    fixed Variants and allows deletiion of current Variants

--- Models/ProductFormModel.cs
+++ Models/ProductFormModel.cs
@@ -1,5 +1,4 @@
 using System.ComponentModel.DataAnnotations;
-// Make sure to add the using for your Data folder where Enums live
 using PuppetFestAPP.Web.Data; 
 
 namespace PuppetFestAPP.Web.Models;
@@ -18,7 +17,7 @@ public class ProductFormModel
     public string? Description { get; set; }
     public int? ParentProductId { get; set; }
     
-    // Add these back as the actual Enum types
-    public ProductSize? Size { get; set; }
-    public ProductColor? Color { get; set; }
+    // Only define these once!
+    public ProductSize? Size { get; set; } = ProductSize.NA;
+    public ProductColor? Color { get; set; } = ProductColor.NA;
 }
